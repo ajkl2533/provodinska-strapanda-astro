0d295f1fae004b800ce9aca4572f2a5c0fee4558
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-02-22 20:58:55 +0100

    Initialize Astro project

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
