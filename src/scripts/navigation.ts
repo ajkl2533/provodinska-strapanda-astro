0b28708a2576c40d683ab8cae945655d2ec2be68
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-16 19:58:25 +0100

    Add navigation

---
+++ src/scripts/navigation.ts
@@ -0,0 +1,36 @@
+const OPEN_CLASS = 'open';
+function hasOpenClass(classes: DOMTokenList | undefined) {
+  return classes?.contains(OPEN_CLASS);
+}
+
+function toggleNav(forceClose?: boolean) {
+  const headerClassList = document?.querySelector('#main-header')?.classList;
+
+  headerClassList?.toggle(OPEN_CLASS);
+
+  if (hasOpenClass(headerClassList)) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+
+  if (forceClose) {
+    [...document.querySelectorAll('.astronav-toggle')].forEach((el) => {
+      el.classList.toggle('hidden');
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.getElementById('astronav-menu');
+  menuButton?.addEventListener('click', () => {
+    toggleNav();
+  });
+
+  const menuListItems = document.querySelectorAll('.nav-list-item');
+  menuListItems?.forEach((el) => {
+    el.addEventListener('click', () => {
+      toggleNav(true);
+    });
+  });
+});
