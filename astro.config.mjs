309a8df995e73bab13585997965bae15c73c3121
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-18 16:51:12 +0100

    Optimize assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import partytown from '@astrojs/partytown';
+import critters from "astro-critters";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    astroImageTools,
-    partytown({
-      config: { forward: ['dataLayer.push'] },
-    }),
-  ],
+  integrations: [astroImageTools, partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), critters()]
 });
