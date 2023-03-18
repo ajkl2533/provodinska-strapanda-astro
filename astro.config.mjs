7d926b306eca92e093eb40552c5afeed23c1bf12
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-18 16:53:12 +0100

    Compress assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { astroImageTools } from 'astro-imagetools';
 import partytown from '@astrojs/partytown';
 import critters from "astro-critters";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroImageTools, partytown({
     config: {
       forward: ['dataLayer.push']
     }
-  }), critters()]
+  }), critters(), compress()]
 });
