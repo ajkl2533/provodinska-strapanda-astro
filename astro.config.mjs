62e7bb224b16f5f032d44fdce542ee7bfcb8fdbb
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-18 18:12:54 +0100

    Use local fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import partytown from '@astrojs/partytown';
-import critters from "astro-critters";
-
-import compress from "astro-compress";
+import critters from 'astro-critters';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), critters(), compress()]
+  integrations: [
+    astroImageTools,
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    critters(),
+    compress(),
+  ],
 });
