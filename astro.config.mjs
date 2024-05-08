18e1e149622ac0f1097b26efa7c390d75e5f53b3
Radek Podrazky <radek.podrazky@gmail.com>
2024-05-08 13:32:49 +0200

    Update track descriptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
-import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
+// import partytown from '@astrojs/partytown';
+import vercel from '@astrojs/vercel/serverless';
+
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
   scopedStyleStrategy: 'where',
   integrations: [
     astroImageTools,
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-        lib: '/_partytown/',
-      },
-    }),
-    compress(),
+    // partytown({
+    //   config: {
+    //     forward: ['dataLayer.push'],
+    //     lib: '/_partytown/',
+    //   },
+    // }),
+    playformCompress(),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
