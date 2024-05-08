4fe718411be1c5288c15283d96ebd0d145be17ff
Radek Podrazky <radek.podrazky@gmail.com>
2024-05-08 19:16:27 +0200

    Remove Partytown and GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   scopedStyleStrategy: 'where',
-  integrations: [
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-        lib: '/_partytown/',
-      },
-    }),
-  ],
   output: 'static',
   adapter: vercel({
     webAnalytics: {
